040bcac9a329d40c008da6dfedfd188db4f4141c
Brendon J. Brewer <brendon.brewer@gmail.com>
2016-10-12 12:44:05 +1300

    Get rid of statics

--- code/MyConditionalPrior.cpp
+++ code/MyConditionalPrior.cpp
@@ -13,7 +13,7 @@ MyConditionalPrior::MyConditionalPrior()
 
 void MyConditionalPrior::from_prior(DNest4::RNG& rng)
 {
-    static const DNest4::Cauchy c;
+    const DNest4::Cauchy c;
     sigma = std::abs(c.generate(rng));
 
     mu_mass = exp(log(1E-3) + log(1E6)*rng.rand());
@@ -25,7 +25,7 @@ double MyConditionalPrior::perturb_hyperparameters(DNest4::RNG& rng)
 	double logH = 0.0;
 
     int which = rng.rand_int(3);
-    static const DNest4::Cauchy c;
+    const DNest4::Cauchy c;
 
     if(which == 0)
     {
@@ -70,7 +70,7 @@ double MyConditionalPrior::log_pdf(const std::vector<double>& vec) const
 #include <iostream>
 void MyConditionalPrior::from_uniform(std::vector<double>& vec) const
 {
-    static const boost::math::normal standard_normal(0.0, 1.0);
+    const boost::math::normal standard_normal(0.0, 1.0);
     vec[0] = sigma*quantile(standard_normal, vec[0]);
     vec[1] = sigma*quantile(standard_normal, vec[1]);
     vec[2] = -mu_mass*log(1.0 - vec[2]);
@@ -79,7 +79,7 @@ void MyConditionalPrior::from_uniform(std::vector<double>& vec) const
 
 void MyConditionalPrior::to_uniform(std::vector<double>& vec) const
 {
-    static const boost::math::normal standard_normal(0.0, 1.0);
+    const boost::math::normal standard_normal(0.0, 1.0);
     vec[0] = cdf(standard_normal, vec[0]/sigma);
     vec[1] = cdf(standard_normal, vec[1]/sigma);
     vec[2] = 1.0 - exp(-vec[2]/mu_mass);
